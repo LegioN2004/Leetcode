081d4a9bbf4941895449dec1c5cee3417f167872
Milinda Barua <milindabarua04@gmail.com>
2026-03-27 01:28:25 +0530

    [1672] [Time Beats: 15.23%] [Memory Beats: 100.00%] - LeetPush

--- Richest-Customer-Wealth.java
+++ Richest-Customer-Wealth.java
@@ -1,13 +1,15 @@
 1class Solution {
 2    public int maximumWealth(int[][] accounts) {
-3        int count = 0;
-4       for (int[] account: accounts) {
-5        int sum = 0;
-6           for (int main: account){
-7            sum += main;
-8           } 
-9           count = Math.max(count, sum);
-10       }
-11       return count;
+3    int max = 0;
+4    for (int m = 0; m < accounts.length; m++) {
+5        int b = 0;
+6        for (int n = 0; n < accounts[m].length; n++) {
+7        b = b + accounts[m][n];
+8          if(b > max) {
+9              max = b;
+10          }
+11      }
 12    }
-13}
+13    return max;
+14    }
+15}
