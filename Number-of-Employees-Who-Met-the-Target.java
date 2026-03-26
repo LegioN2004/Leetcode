6e951f84c70f77a626867ac1475aa689ee404e1f
Milinda Barua <milindabarua04@gmail.com>
2026-03-27 01:27:44 +0530

    [2798] [Time Beats: 24.78%] [Memory Beats: 100.00%] - LeetPush

--- Number-of-Employees-Who-Met-the-Target.java
+++ Number-of-Employees-Who-Met-the-Target.java
@@ -1,16 +1,11 @@
 1class Solution {
 2    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
 3        int count = 0;
-4        // for(int i = 0; i < hours.length; i++){
-5        //     if(hours[i] >= target){
-6        //         count++;
-7        //     }
-8        // }
-9        // better solution
-10        for(int hour: hours){
-11            if(hour >= target)
-12                count++;
-13        }
-14        return count;
-15    }
-16}
+4        for(int i = 0; i < hours.length; i++){
+5            if(hours[i] >= target){
+6                count++;
+7            }
+8        }
+9        return count;
+10    }
+11}
