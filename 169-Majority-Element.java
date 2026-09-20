d9c8d5fafdce3e6cba048bff071742a1fe839b69
Milinda Barua <milindabarua04@gmail.com>
2026-09-20 06:57:37 +0530

    [169] [Time Beats: 71.46%] [Memory Beats: 5.14%] - LeetPush

--- 169-Majority-Element.java
+++ 169-Majority-Element.java
@@ -13,18 +13,6 @@ class Solution {
                 count--;
             }
         }
-
-        int count1 = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == element) {
-                count1++;
-            }
-        }
-        if (count1 > (nums.length / 2)) {
-            return element;
-        }
-
-        return -1;
+        return element;
     }
 }
